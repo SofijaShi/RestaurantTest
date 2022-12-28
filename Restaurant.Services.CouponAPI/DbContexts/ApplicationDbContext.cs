1d4b51c903c35cb89ed769e10f4ccc9f3906e9a4
LAPTOP-OJ757MOJ\Sofija <sofija.shishevska@learnlight.com>
2022-12-28 21:37:36 +0100

    Seed coupon db, CouponController and CouponRepository added

--- Restaurant.Services.CouponAPI/DbContexts/ApplicationDbContext.cs
+++ Restaurant.Services.CouponAPI/DbContexts/ApplicationDbContext.cs
@@ -11,5 +11,26 @@ namespace Restaurant.Services.CouponAPI.DbContexts
         }
 
         public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "100FF",
+                DiscountAmount = 10
+            });
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "300FF",
+                DiscountAmount = 30
+            });
+
+
+        }
     }
 }
---
+++ Restaurant.Services.CouponAPI/Repository/CouponRepository.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Restaurant.Services.CouponAPI.DbContexts;
+using Restaurant.Services.CouponAPI.Models;
+using Restaurant.Services.CouponAPI.Models.Dto;
+
+namespace Restaurant.Services.CouponAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private IMapper _mapper;
+
+        public CouponRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+        public async Task<CouponDto> GetCouponByCode(string couponCode)
+        {
+            Coupon coupon = _db.Coupons.Where(x => x.CouponCode == couponCode).FirstOrDefault();
+            return _mapper.Map<CouponDto>(coupon);
+        }
+    }
+}
